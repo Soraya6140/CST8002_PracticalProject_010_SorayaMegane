5430c74c048369187c5b5a9fc5409f17d4ea2279
Soraya6140 <kadjisoraya@gmail.com>
2026-07-27 00:12:37 -0400

    Remove target folder from tracking, add .gitignore

--- target/debug/build/serde_core-fe348a2ff673a39d/build_script_build-fe348a2ff673a39d.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ekenj\OneDrive\Documents\pp2\target\debug\build\serde_core-fe348a2ff673a39d\build_script_build-fe348a2ff673a39d.d: C:\Users\ekenj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.229\build.rs
-
-C:\Users\ekenj\OneDrive\Documents\pp2\target\debug\build\serde_core-fe348a2ff673a39d\build_script_build-fe348a2ff673a39d.exe: C:\Users\ekenj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.229\build.rs
-
-C:\Users\ekenj\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.229\build.rs:
--- target/debug/deps/pp2-d3d48db29534a2f2.d
+++
@@ -1,9 +0,0 @@
-C:\Users\ekenj\OneDrive\Documents\pp2\target\debug\deps\pp2-d3d48db29534a2f2.d: src\main.rs src\business\mod.rs src\model\mod.rs src\persistence\mod.rs src\presentation\mod.rs
-
-C:\Users\ekenj\OneDrive\Documents\pp2\target\debug\deps\pp2-d3d48db29534a2f2.exe: src\main.rs src\business\mod.rs src\model\mod.rs src\persistence\mod.rs src\presentation\mod.rs
-
-src\main.rs:
-src\business\mod.rs:
-src\model\mod.rs:
-src\persistence\mod.rs:
-src\presentation\mod.rs:
